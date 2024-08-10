f7d1cc4359f93d6f39761ab13bec599a045cf7ab
Ian Gordon <ianbgordon@gmail.com>
2024-08-10 17:24:06 -0400

    add edge cases for exponenntial, jump, and interpolation search. Add metric cli argument

--- search.py
+++ search.py
@@ -3,8 +3,8 @@ import random
 import time
 
 import matplotlib.pyplot as plt
+import numpy
 from scipy import stats
-from scipy.ndimage import median
 
 # Search algorithms
 
@@ -84,6 +84,9 @@ def ternary_search_recursive(arr, target):
 
 
 def exponential_search(arr, target):
+    if len(arr) == 0:
+        return -1
+
     if arr[0] == target:
         return 0
 
@@ -95,6 +98,9 @@ def exponential_search(arr, target):
 
 
 def jump_search(arr, target):
+    if len(arr) == 0:
+        return -1
+
     n = len(arr)
     jmp = int(n**0.5)
     step = jmp
@@ -119,7 +125,7 @@ def jump_search(arr, target):
 def interpolation_search(arr, target):
     low = 0
     high = len(arr) - 1
-    while low <= high and arr[low] <= target < arr[high]:
+    while low < high and arr[low] <= target < arr[high]:
         mid = low + ((target - arr[low]) * (high - low)) // (arr[high] - arr[low])
         if arr[mid] < target:
             low = mid + 1
@@ -127,7 +133,7 @@ def interpolation_search(arr, target):
             high = mid - 1
         else:
             return mid
-    return low if arr[low] == target else -1
+    return high
 
 
 def fibonacci_search(arr, target):
@@ -171,6 +177,28 @@ def get_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
+        "--test",
+        type=str,
+        nargs="*",
+        default=[fn for fn in fns_by_name],
+        help="Algorithms to include in testing",
+    )
+
+    parser.add_argument(
+        "--min",
+        type=int,
+        default=0,
+        help="Start list size",
+    )
+
+    parser.add_argument(
+        "--max",
+        type=int,
+        default=1500,
+        help="End list size",
+    )
+
+    parser.add_argument(
         "--resolution",
         type=int,
         default=50,
@@ -178,24 +206,17 @@ def get_args():
     )
 
     parser.add_argument(
-        "--start",
-        type=int,
-        default=1,
-        help="Start list size",
-    )
-
-    parser.add_argument(
-        "--stop",
-        type=int,
-        default=1500,
-        help="End list size",
-    )
-
-    parser.add_argument(
         "--n_trials",
         type=int,
         default=50,
         help="Number of trials to average over",
+    )
+
+    parser.add_argument(
+        "--metric",
+        type=str,
+        default="median",
+        help="Metric measured across trials",
     )
 
     parser.add_argument(
@@ -221,29 +242,24 @@ def get_args():
         help="Algorithms to exclude from testing",
     )
 
-    parser.add_argument(
-        "--test",
-        type=str,
-        nargs="*",
-        default=[fn for fn in fns_by_name],
-        help="Algorithms to include in testing",
-    )
-
     return parser.parse_args()
 
 
-def time_functions(fns_by_name, n_trials, list_sizes, rand_fn):
+def time_functions(fns_by_name, n_trials, list_sizes, rand_fn, metric):
     fn_times = {name: len(list_sizes) * [[]] for name in fns_by_name}
 
     for i, list_size in enumerate(list_sizes):
         for j in range(n_trials):
+            j_justified = str(j + 1).rjust(len(str(n_trials)), " ")
+            i_justified = str(i + 1).rjust(len(str(len(list_sizes))), " ")
+            size_justified = str(list_size).rjust(len(str(list_sizes[-1])), " ")
             print(
-                f"\33[2K\rTiming trial {j+1}/{n_trials} on list size {i+1}/{len(list_sizes)} (curr: {list_size}, max: {list_sizes[-1]})",
+                f"\33[2K\rTiming trial {j_justified}/{n_trials} on list size {i_justified}/{len(list_sizes)} (curr: {size_justified}, max: {list_sizes[-1]})",
                 end="",
             )
 
             lst = [rand_fn() for _ in range(list_size)]
-            tgt = random.choice(lst)
+            tgt = 0 if list_size == 0 else random.choice(lst)
             lst.sort()
             for name, fn in fns_by_name.items():
                 cpy = lst.copy()
@@ -253,16 +269,16 @@ def time_functions(fns_by_name, n_trials, list_sizes, rand_fn):
                 fn_times[name][i].append(end - start)
 
         for fn in fn_times:
-            fn_times[fn][i] = median(fn_times[fn][i])
+            fn_times[fn][i] = metric(fn_times[fn][i])
 
     return fn_times
 
 
-def plot_times(list_sizes, fn_times):
+def plot_times(list_sizes, fn_times, metric):
     for name, times in fn_times.items():
         plt.plot(list_sizes, times, label=name)
-    plt.ylabel("Median Time (s)")
-    plt.xlabel("List Size")
+    plt.ylabel(f"{metric} time (s)")
+    plt.xlabel("list size")
     plt.legend()
     plt.show()
 
@@ -282,12 +298,16 @@ if __name__ == "__main__":
         for name, fn in fns_by_name.items()
         if name in args.test and name not in args.blacklist
     }
-    step = (args.stop - args.start) // args.resolution
-    list_sizes = [sz for sz in range(args.start, args.stop, step)]
+    step = (args.max - args.min) // args.resolution
+    list_sizes = [
+        round(i * (args.max - args.min) / args.resolution + args.min)
+        for i in range(args.resolution + 1)
+    ]
     distribution = getattr(stats, args.distribution)(*args.params)
     rand_fn = lambda: distribution.rvs()
+    metric = getattr(numpy, args.metric)
 
     print("\nTiming functions")
-    fn_times = time_functions(fns, args.n_trials, list_sizes, rand_fn)
-    plot_times(list_sizes, fn_times)
+    fn_times = time_functions(fns, args.n_trials, list_sizes, rand_fn, metric)
+    plot_times(list_sizes, fn_times, args.metric)
     print()
